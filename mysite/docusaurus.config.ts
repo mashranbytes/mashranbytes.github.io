68d9c71de7b59b5281e57e5aedd6722302ca9be9
mashranbytes <mashranbytes@gmail.com>
2026-06-27 02:22:50 +0400

    Updated the config of the sidebars to have multiple sidebars

--- mysite/docusaurus.config.ts
+++ mysite/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Mashranbytes',
+  tagline: 'Learning is cool',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://mashranbytes.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mashranbytes', // Usually your GitHub org/user name.
+  projectName: 'mashranbytes.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -87,6 +87,12 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar2',
+          position: 'left',
+          label: 'Tutorial2',
+      },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
